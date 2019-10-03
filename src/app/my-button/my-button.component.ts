cc8dbfc7d59668ab02e36fc9b95ce5fbd625ec61
craig <crgtrr@gmail.com>
2019-10-02 21:24:19 -0500

    added image button and span

--- src/app/my-button/my-button.component.ts
+++ src/app/my-button/my-button.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LikeImageService } from './../like-image.service';
 
 @Component({
   selector: 'app-my-button',
@@ -6,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./my-button.component.css']
 })
 export class MyButtonComponent implements OnInit {
-
-  constructor() { }
-
+  liked: boolean;
+  constructor( likeImageService: LikeImageService ) {
+          this.liked = false;
+  }
+  handleClick(event: Event) {
+    if (this.liked === false) {
+      this.liked = true;
+    } else {
+      this.liked = false;
+    }
+    console.log('Button clicked', event);
+  }
   ngOnInit() {
   }
 
